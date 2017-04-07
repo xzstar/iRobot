9b4b6f80ea1a77e9d66dfaa2f464166b0eb29f47
xiezhe <xiezhe@lianjia.com>
2017-04-07 22:44:05 +0800

    test: use current timestamp to test

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python
+#!/Library/Frameworks/Python.framework/Versions/3.5/bin/python3
 import redis
-
+import time
 channels = ['deal','timebarinfo','heartbeat']
 
 redisconn = redis.StrictRedis()
 redisconn.publish(channels[0],'{"user":"xiezhe","instrument":"rb1710","direction":"1","holders":"2","price":"1000","time":"2017-1-1"}')
 redisconn.publish(channels[1],'{"user":"xiezhe","instrument":"rb1710","price":"1000","time":"2017-1-1"}')
-redisconn.publish(channels[2],'{"user":"xiezhe","timestamp":1491312733}')
+timestamp = int(time.time())
+redisconn.publish(channels[2],'{"user":"xiezhe","timestamp":timestamp}')
